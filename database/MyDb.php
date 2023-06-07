0393944cca8af1bc4e9ef3be532543c54279d7b0
Daniel Galion <daniel.galion94@gmail.com>
2023-06-07 18:12:29 +0200

    Database connection
    Singleton for getting mysqli with placeholder abstract class for login data.

---
+++ database/MyDb.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database;
+
+use mysqli;
+
+/**
+ * @todo test with started and stopped mariadb
+ * Class is singleton for not creating more than one instance of mysqli to make connection.
+ */
+final class MyDb {
+    private static ?mysqli $instance = null;
+    
+    private function __construct()
+    {
+        
+    }
+
+    private function __clone()
+    {
+        
+    }
+
+    private function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize DB object.");
+    }
+
+    public static function getInstance(): mysqli {
+        if (self::$instance !== null) {
+            return self::$instance;
+        } else { 
+            self::$instance = self::createDatabaseConnection();
+        }
+
+        return self::$instance;
+    }
+
+    private static function createDatabaseConnection(): mysqli {
+        $connection = new mysqli(ConnectionDetails::HOST, ConnectionDetails::LOGIN, ConnectionDetails::PASSWORD, ConnectionDetails::DATABASE, ConnectionDetails::PORT);
+
+        if ($connection->connect_error) {
+            die("DB connection error: ". $connection->connect_error);
+        }
+
+        return $connection;
+    } 
+}
