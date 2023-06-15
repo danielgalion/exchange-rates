485604ada2e79b08c2ca9f8bb04b62efc294b26e
Daniel Galion <daniel.galion94@gmail.com>
2023-06-15 05:16:26 +0200

    View of exchange history

--- app/src/views/ExchangeHistoryView.php
+++ app/src/views/ExchangeHistoryView.php
@@ -15,10 +15,33 @@ require_once 'vendor/autoload.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../style.css">
     <title>Historia wymiany walut</title>
 </head>
 <body>
-    <h1>Ostatnie przewalutowania</h1>
-    <?= var_dump((new Trade)->getLast(3)) ?>
+    <h1>Ostatnie maksymalnie 20 przewalutowań</h1>
+    <!-- <?= var_dump((new Trade)->getLast(3)) ?> -->
+
+    <?php $trades = (new Trade)->getLast(20); ?>
+    <table>
+        <tr>
+            <td>Waluta sprzedana</td>
+            <td>Waluta kupiona</td>
+            <td>Cena sprzedaży (zł)</td>
+            <td>Cena kupna (zł)</td>
+            <td>Ilość sprzedana</td>
+            <td>Ilość kupiona</td>
+        </tr>
+        <?php foreach ($trades as $trade): ?>
+            <tr>
+                <td><?= $trade['sold'] ?></td>
+                <td><?= $trade['bought'] ?></td>
+                <td><?= $trade['selling_price'] ?></td>
+                <td><?= $trade['buying_price'] ?></td>
+                <td><?= $trade['amount_sold'] ?></td>
+                <td><?= $trade['amount_bought'] ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </body>
 </html>
