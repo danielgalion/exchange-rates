81b98ed17185a6871ca623155a0bc53de559837d
Daniel Galion <daniel.galion94@gmail.com>
2023-06-15 05:51:08 +0200

    Deploy version

---
+++ app/database/ConnectionDetails.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Database;
+ 
+abstract class ConnectionDetails { 
+    const HOST = 'localhost';
+    const LOGIN = 'danielg';
+    const PASSWORD = 'D4n13l13';
+    const DATABASE = 'adrespect_rates';
+    const PORT = 3306;
+}
